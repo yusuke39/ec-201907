92c3eb6ca51385ebd5204de7c6707b4d4f987a59
yusuke39 <hiranoyusuke39@gmail.com>
2019-08-30 14:51:30 +0900

    stasicfolder and img 追加

--- src/main/java/com/example/service/showItemListService.java
+++ src/main/java/com/example/service/showItemListService.java
@@ -4,10 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-<<<<<<< HEAD
+
 import org.springframework.transaction.annotation.Transactional;
-=======
->>>>>>> feature/showItemDetail
+
 
 import com.example.domain.Item;
 import com.example.repository.ItemRepository;
