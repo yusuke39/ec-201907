c464dbe1c0846eb21451a519ea95e1da217f4e55
junyayoshioka <yoshiokajunya@yoshiokajunyanoMacBook-Pro.local>
2019-08-30 17:00:30 +0900

    登録画面作成（エラー表示はされません）

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -1,5 +1,30 @@
 package com.example.repository;
 
-public class UserRepository {
 
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.User;
+
+/**
+ * @author junya.yoshioka
+ *
+ */
+@Repository
+public class UserRepository {
+	
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+	
+	
+	public void insert(User user) {
+		SqlParameterSource param = new BeanPropertySqlParameterSource(user);
+		String sql = "INSERT INTO users(name, email, password, zipcode, address, telephone) VALUES(:name, :email, :password, :zipcode, :address, :telephone)";
+		template.update(sql, param);
+	}
 }
