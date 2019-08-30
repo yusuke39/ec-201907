3304caf66fca9aae65ec335ed515ce3129eec5ed
yusuke39 <hiranoyusuke39@gmail.com>
2019-08-30 14:30:02 +0900

    商品詳細表示contoroller,service,repositoryの作成

--- src/main/java/com/example/service/showItemDetailService.java
+++ src/main/java/com/example/service/showItemDetailService.java
@@ -1,22 +1,22 @@
 package com.example.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.domain.Item;
 import com.example.repository.ItemRepository;
 
-
 @Service
-public class showItemListService {
+public class showItemDetailService {
 	
 	@Autowired
 	private ItemRepository itemRepository;
 	
-	public List<Item> findAllItems(){
-		return itemRepository.findAll();
+	public Item findItem(Integer itemId) {
+		
+		Item showItemDetail = itemRepository.load(itemId);
+		
+		return showItemDetail;
 	}
 
 }
