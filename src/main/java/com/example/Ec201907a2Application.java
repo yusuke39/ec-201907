a77065ced5ec46adae7db940517685f7f6054fb5
hiroki-okazaki <1031okazaki@gmail.com>
2019-08-30 12:50:54 +0900

    first commit

---
+++ src/main/java/com/example/Ec201907a2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec201907a2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec201907a2Application.class, args);
+	}
+
+}
