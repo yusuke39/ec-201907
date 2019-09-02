6859bcd232ba57bc7efc8059336010b76df85305
hiroki-okazaki <1031okazaki@gmail.com>
2019-09-02 17:25:09 +0900

    注文履歴HTML実装(未完成)

--- src/main/java/com/example/controller/ShowUserOrderHistoryController.java
+++ src/main/java/com/example/controller/ShowUserOrderHistoryController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Order;
@@ -26,9 +27,11 @@ public class ShowUserOrderHistoryController {
 	}
 	
 	@RequestMapping("/orderHistoryList")
-	public String loadUserOrderHistory(Integer userId) {
-		List<Order> orderList = orderHistoryRepository.findAll();
-		System.out.println(orderList);
+	public String loadUserOrderHistory(Integer userId,Model model) {
+		List<Order> orderHistoryList = orderHistoryRepository.findAll();
+		System.out.println(orderHistoryList);
+		
+		model.addAttribute("orderHistoryList", orderHistoryList);
 		return "order_history";
 	}
 	
