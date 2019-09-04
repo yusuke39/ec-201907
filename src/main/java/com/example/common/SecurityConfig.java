fd66394c3f6c29ab6eaf9b070bbc0174bf83872e
hiroki-okazaki <1031okazaki@gmail.com>
2019-09-04 13:35:58 +0900

    ログアウト処理修正

--- src/main/java/com/example/common/SecurityConfig.java
+++ src/main/java/com/example/common/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.passwordParameter("password"); // 認証時に使用するパスワードのリクエストパラメータ名
 		
 		http.logout() // ログアウトに関する設定
-			.logoutRequestMatcher(new AntPathRequestMatcher("logout**")) // ログアウトさせる際に遷移させるパス
+			.logoutRequestMatcher(new AntPathRequestMatcher("/logout**")) // ログアウトさせる際に遷移させるパス
 			.logoutSuccessUrl("/index/showItemList") // ログアウト後に遷移させるパス(ここではログイン画面を設定)
 			.deleteCookies("JSESSIONID") // ログアウト後、Cookieに保存されているセッションIDを削除
 			.invalidateHttpSession(true); // true:ログアウト後、セッションを無効にする false:セッションを無効にしない
