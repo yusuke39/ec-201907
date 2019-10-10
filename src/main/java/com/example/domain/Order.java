ea9cc39cfab68c10d5df542ae73214950d642f2b
yusuke39 <hiranoyusuke39@gmail.com>
2019-10-10 23:17:19 +0900

    訂正

--- src/main/java/com/example/domain/Order.java
+++ src/main/java/com/example/domain/Order.java
@@ -1,6 +1,5 @@
 package com.example.domain;
 
-
 /**
  * Timestampの参照型を利用するに伴い
  * java.security.Timestampをインポートしているが
@@ -14,13 +13,10 @@ package com.example.domain;
  *
  */
 
-
 import java.util.Date;
 import java.util.List;
 
 public class Order {
-
-
 
 	/**
 	 * ID(主キー)
@@ -78,9 +74,7 @@ public class Order {
 	 * OrderItem型のList
 	 */
 	private List<OrderItem> orderItemList;
-	
-	
-	
+
 	@Override
 	public String toString() {
 		return "Order [id=" + id + ", userId=" + userId + ", status=" + status + ", totalPrice=" + totalPrice
@@ -89,112 +83,137 @@ public class Order {
 				+ destinationAddress + ", destinationTel=" + destinationTel + ", deliveryTime=" + deliveryTime
 				+ ", paymentMethod=" + paymentMethod + ", user=" + user + ", orderItemList=" + orderItemList + "]";
 	}
-	
-	
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public Integer getUserId() {
 		return userId;
 	}
+
 	public void setUserId(Integer userId) {
 		this.userId = userId;
 	}
+
 	public Integer getStatus() {
 		return status;
 	}
+
 	public void setStatus(Integer status) {
 		this.status = status;
 	}
+
 	public Integer getTotalPrice() {
 		return totalPrice;
 	}
+
 	public void setTotalPrice(Integer totalPrice) {
 		this.totalPrice = totalPrice;
 	}
+
 	public Date getOrderDate() {
 		return orderDate;
 	}
+
 	public void setOrderDate(Date orderDate) {
 		this.orderDate = orderDate;
 	}
+
 	public String getDestinationName() {
 		return destinationName;
 	}
+
 	public void setDestinationName(String destinationName) {
 		this.destinationName = destinationName;
 	}
-	
+
 	public String getDestinationEmail() {
 		return destinationEmail;
 	}
+
 	public void setDestinationEmail(String destinationEmail) {
 		this.destinationEmail = destinationEmail;
 	}
+
 	public String getDestinationZipcode() {
 		return destinationZipcode;
 	}
+
 	public void setDestinationZipcode(String destinationZipcode) {
 		this.destinationZipcode = destinationZipcode;
 	}
+
 	public String getDestinationAddress() {
 		return destinationAddress;
 	}
+
 	public void setDestinationAddress(String destinationAddress) {
 		this.destinationAddress = destinationAddress;
 	}
+
 	public String getDestinationTel() {
 		return destinationTel;
 	}
+
 	public void setDestinationTel(String destinationTel) {
 		this.destinationTel = destinationTel;
 	}
+
 	public java.sql.Timestamp getDeliveryTime() {
 		return deliveryTime;
 	}
+
 	public void setDeliveryTime(java.sql.Timestamp timestamp) {
 		this.deliveryTime = timestamp;
 	}
+
 	public Integer getPaymentMethod() {
 		return paymentMethod;
 	}
+
 	public void setPaymentMethod(Integer paymentMethod) {
 		this.paymentMethod = paymentMethod;
 	}
+
 	public User getUser() {
 		return user;
 	}
+
 	public void setUser(User user) {
 		this.user = user;
 	}
+
 	public List<OrderItem> getOrderItemList() {
 		return orderItemList;
 	}
+
 	public void setOrderItemList(List<OrderItem> orderItemList) {
 		this.orderItemList = orderItemList;
 	}
-	
+
 	/**
 	 * 合計金額のTAX処理.
 	 * 
 	 * @return tax(消費税額)
 	 */
 	public int getTax() {
-		return (int)(getCalcTotalPrice() * 0.08);
+		return (int) (getCalcTotalPrice() * 0.08);
 	}
-	
+
 	/**
 	 * 
 	 * 
 	 * @return includeTaxTotalPrice(税込合計金額)
 	 */
 	public int getCalcTotalPrice() {
-		
+
 		int totalPrice = 0;
-		for(OrderItem orderItem : orderItemList) {
+		for (OrderItem orderItem : orderItemList) {
 			totalPrice += orderItem.getSubTotal();
 		}
 		return totalPrice;
